53ea66bd11c76f841f5bd3c22c2d073fb6708431
Aayush <aayushbisht501@gmail.com>
2025-03-31 18:12:08 +0530

    auth pages setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,8 @@ import React, { ReactNode } from 'react'
 
 const AuthLayout = ({children}:{children:ReactNode}) => {
   return (
-    <div>{children}</div>
-  )
+    <div className='auth-layout'>{children}</div>
+  ) 
 }
 
 export default AuthLayout
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,8 +1,93 @@
-import React from 'react'
+"use client"
 
-const AuthForm = () => {
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+import { Form } from "./ui/form"
+import Link from "next/link"
+import { toast } from "sonner"
+import FormField from "./FormField"
+import { useRouter } from "next/navigation"
+
+
+
+const authFormSchema = (type:FormType) =>{
+    return z.object({
+        name:type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(3),
+    })
+}
+
+
+const AuthForm = ({type}:{type:FormType}) => {
+  const router = useRouter();
+    const formSchema = authFormSchema(type);
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          name: "",
+          email:"",
+          password:"",
+        },
+      })
+     
+      // 2. Define a submit handler.
+      function onSubmit(values: z.infer<typeof formSchema>) {
+        try{
+            if(type==='sign-up'){
+                console.log('Sign up',values)
+                toast.success('Account created Successfully. Please sign in')
+                router.push('/sign-in')
+
+            }
+            else{
+                console.log('sign in', values)
+                toast.success('Sign in successfull')
+                router.push('/sign-in')
+            }
+
+        } catch(error){
+            console.log(error);
+            toast.error( `There was an error:${error}`)
+        }
+      }  
+
+      const isSignIn = type === 'sign-in';
   return (
-    <div>AuthForm</div>
+    <div className="card-border lg:min-w-[566px]">
+        <div className="flex flex-col gap-6 card py-14 px-10">
+           <div className="flex flex-row gap-1 justify-center">
+            <Image src="/logo.svg" alt="logo" height={32} width={38} ></Image>
+            <h2 className="text-primary-100">SmartPrep</h2>
+            <h3>Practice job interview with AI</h3>
+           
+        </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+        {!isSignIn && (<FormField 
+        control = {form.control} 
+        name = "name" 
+        label="Name"  type = "name" placeholder ="Your Name"/ >)}
+        <FormField 
+        control = {form.control} 
+        name = "email" 
+        label="Email" placeholder ="Your Email" type = 'email'/>
+        <FormField 
+        control = {form.control} 
+        name = "password" 
+        label="Password"  placeholder ="Your Password" type="password"/>
+        <Button type="submit">{isSignIn ? 'Sign In' : 'Sign Up' }</Button>
+      </form>
+    </Form>
+    <p className="text-center">{isSignIn ? 'No Account yet' : 'Have an account already?'}
+        <Link href={!isSignIn?'/sign-in':'/sign-up'} className="font-bold text-user-primary ml-1"> {!isSignIn?"Sign In":"Sign Up"}</Link>
+    </p>
+    </div>
+    </div> 
+
   )
 }
 
