1a2334f531ac9adb5af0c7e1f23b71433d98d5a7
Aayush <aayushbisht501@gmail.com>
2025-04-08 13:04:29 +0530

    change default path

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint:{
-    ignoreDuringBuilds:true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  typescript:{
-    ignoreBuildErrors:true,
-  }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/landing',
+        permanent: false, 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
